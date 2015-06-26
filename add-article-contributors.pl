d7be8f0bda757e3dfac3b1c0e6c75828d6feaaeb
Brian Duggan <bduggan@usgcrp.gov>
2015-06-26 13:15:43 -0400

    add-article-contributors now posts

--- add-article-contributors.pl
+++ add-article-contributors.pl
@@ -2,16 +2,25 @@
 
 use Gcis::Client;
 use Smart::Comments;
-binmode(STDOUT, ':encoding(utf8)');
 use Data::Dumper;
 use YAML::XS qw/Dump/;
+
 use v5.16;
 use experimental 'signatures';
+
+binmode(STDOUT, ':encoding(utf8)');
+
+my $all = 1;         # all articles or just the first 20?
+my $just_dump = 0;   # just dump the list or change GCIS?
 
 my $url = shift || die 'missing url';
 my $gcis  = Gcis::Client->connect(url => $url);
 my $orcid = Gcis::Client->new->url("http://pub.orcid.org")->accept("application/orcid+json");
 my $xref  = Gcis::Client->new->url("http://dx.doi.org")->accept("application/vnd.citationstyles.csl+json;q=0.5");
+
+sub debug($) {
+    # warn "# @_\n";
+}
 
 sub get_orcid_authors($doi) {
     my @authors;
@@ -55,14 +64,65 @@ sub combine_author_list($some,$all) {
     return \@authors;
 }
 
-for my $article ($gcis->get("/article", { all => 1 })) {    ### Getting--->   done
+sub find_or_create_gcis_person($person) {
+    my $match;
+
+    # ORCID
+    if ($person->{orcid} and $match = $gcis->get("/person/$person->{orcid}")) {
+        debug "Found orcid: $person->{orcid}";
+        return $match;
+    }
+
+    # Match first + last name
+    if ($match = $gcis->post_quiet("/person/lookup/name",
+            { last_name => $person->{last_name},
+              first_name => $person->{first_name}
+          })) {
+        if ($match->{id}) {
+            return $match;
+        }
+    }
+
+    # Add more heuristics here
+
+    debug "adding new person $person->{first_name} $person->{last_name}";
+    my $new = $gcis->post("/person" => {
+            first_name => $person->{first_name},
+             last_name => $person->{last_name},
+                 orcid => $person->{orcid}
+            }) or do {
+            warn "Error creating ".Dumper($person)." : ".$gcis->error;
+            return;
+        };
+
+    return $new;
+}
+
+sub add_contributor_record($person,$article) {
+    my $uri = $article->{uri};
+    $uri =~ s[article][article/contributors];
+    $gcis->post( $uri => {
+            person_id => $person->{id},
+            role => 'author'
+        }) or debug "error posting to $uri: ".$gcis->error;
+}
+
+for my $article ($gcis->get("/article", { all => $all })) {    ### Getting--->   done
     my $doi = $article->{doi} or next;
     my $some = get_orcid_authors($doi);
     my $all = get_xref_authors($doi) or die "no authors for $doi";
     my $merged = combine_author_list($some,$all) or next;
-    printf "%100s\n",$doi;
-    for (@$merged) {
-        printf "%-25s %-30s %-30s\n",$_->{orcid},$_->{first_name},$_->{last_name};
+    if ($just_dump) {
+        printf "%100s\n",$doi;
+        for (@$merged) {
+            printf "%-25s %-30s %-30s\n",$_->{orcid},$_->{first_name},$_->{last_name};
+        }
+        next;
+    }
+    for my $person (@$merged) {
+        my $found = find_or_create_gcis_person($person);
+        next unless $found;
+        add_contributor_record($found, $article);
     }
 }
 
