b3aba1b6f7da979ebc920482c10e61f77da1d545
Brian Duggan <bduggan@usgcrp.gov>
2015-08-06 09:41:02 -0400

    checksum for issns

--- check-issns.pl
+++ check-issns.pl
@@ -1,10 +1,19 @@
 #!/usr/bin/env perl
 use v5.14;
 use Gcis::Client;
+use Business::ISSN qw/is_valid_checksum/;
 
 my $url = shift or die "no url";
 my $gcis = Gcis::Client->new(url => $url);
 for my $journal ($gcis->get("/journal?all=1")) {
-    next if $journal->{print_issn} || $journal->{online_issn};
-    say "no issn for $url".$journal->{uri};
+    unless ($journal->{print_issn} || $journal->{online_issn}) {
+        say "No issn for $url";
+        next;
+    }
+    if ($journal->{print_issn} && !is_valid_checksum($journal->{print_issn})) {
+        say "Invalid print issn: $journal->{print_issn} for $journal->{uri}";
+    }
+    if ($journal->{online_issn} && !is_valid_checksum($journal->{online_issn})) {
+        say "Invalid online issn: $journal->{online_issn} for $journal->{uri}";
+    }
 }
